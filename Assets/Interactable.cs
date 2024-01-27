965351adb77fc740d29e09b432d6adb072fb8cad
wheelspawn <nsage@protonmail.com>
2024-01-27 14:11:48 -0600

    fix bug

--- Assets/Interactable.cs
+++ Assets/Interactable.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IInteractable : MonoBehaviour
+public interface IInteractable
 {
     // Do the thing
     void Interact() {}
