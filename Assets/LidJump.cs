fde711250a14d9714f961777031133ac9fa59f93
David Priester <dpriester@uiowa.edu>
2024-01-28 00:20:06 -0600

    I don't know what's happening

---
+++ Assets/LidJump.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LidJump : MonoBehaviour, IInteractable
+{
+    public Rigidbody m_Rigidbody;
+    public float m_ForceMagnitude = 2000f; // Adjust the force magnitude as needed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        // Assuming your GameObject has a Collider, you can set its material here
+        // Adjust "BouncyMaterial" to the actual name of your Physics Material
+        Collider collider = GetComponent<Collider>();
+        if (collider != null)
+        {
+            collider.material = new PhysicMaterial("BouncyMaterial");
+            collider.material.bounciness = 1.0f; // 1.0 for maximum bounciness
+        }
+    }
+
+    // Update is called once per frame
+    public void Interact()
+    {
+        Vector3 forceDirection = new Vector3(1f, 1f, 0f).normalized;
+        m_Rigidbody.AddForce(forceDirection * m_ForceMagnitude);
+    }
+}
