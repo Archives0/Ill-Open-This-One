f3b94bb25edfcfeb4fc6a1b58530c72d1c6e4266
wheelspawn <nsage@protonmail.com>
2024-01-27 22:13:45 -0600

    add bumpable script

---
+++ Assets/Scripts/Mechanics/Bumpable.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumpable : MonoBehaviour
+{
+    public float collisionImpulseScale = 1.0f; // Public variable to scale collision impulse.
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        return;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Calculate the collision direction.
+        Vector3 collisionDirection = transform.position - collision.contacts[0].point;
+        collisionDirection.Normalize();
+
+        // Calculate the collision impulse based on the collision velocity.
+        float collisionVelocity = collision.relativeVelocity.magnitude;
+        float impulseMagnitude = collisionVelocity * collisionImpulseScale;
+
+        // Apply the impulse in the opposite direction of the collision.
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForce(collisionDirection * impulseMagnitude, ForceMode.Impulse);
+        }
+    }
+
+}
